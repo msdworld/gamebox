f44790b8103d1b424564f3af31343dc5ea057085
msdworld <75013420+msdworld@users.noreply.github.com>
2023-12-28 00:57:10 +0530

    AddCarData.php

--- AddCarData.php
+++ AddCarData.php
@@ -1,19 +1,31 @@
-<!-- addModel.php -->
 <?php
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     // Retrieve form data
     $brand = $_POST['brand'];
     $modelName = $_POST['modelName'];
+    $price = $_POST['price'];
     $launchDate = $_POST['launchDate'];
     $mileage = $_POST['mileage'];
+    $capacity = $_POST['capacity'];
+    $fuelType = $_POST['fuelType'];
+    $wheelType = $_POST['wheelType'];
+    $postLink = $_POST['postLink'];
 
     // Read existing JSON file
-    $jsonFile = 'https://github.com/msdworld/gamebox/raw/main/CarData.json';
+    $jsonFile = 'https://raw.githubusercontent.com/msdworld/gamebox/main/CarData.json';
     $currentData = json_decode(file_get_contents($jsonFile), true);
 
     // Add new data
     if (!isset($currentData[$brand]['models'][$modelName])) {
-        $currentData[$brand]['models'][$modelName] = ['price' => $price, 'launchDate' => $launchDate, 'mileage' => $mileage, 'capacity' => $capacity, 'fuelType' => $fuelType', 'wheelType' => $wheelType, 'postLink' => $postLink ];
+        $currentData[$brand]['models'][$modelName] = [
+            'price' => $price,
+            'launchDate' => $launchDate,
+            'mileage' => $mileage,
+            'capacity' => $capacity,
+            'fuelType' => $fuelType,
+            'wheelType' => $wheelType,
+            'postLink' => $postLink
+        ];
 
         // Write updated data back to the JSON file
         file_put_contents($jsonFile, json_encode($currentData, JSON_PRETTY_PRINT));
